46bff2b7e011fa6de13780b18804c93b2cfd77be
amaljith c <58605219+amaljithc@users.noreply.github.com>
2021-12-09 15:21:35 +0530

    Add files via upload

---
+++ Exc1.10.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import numpy as num
+x=num.array([[1,2,3],[4,5,6],[7,8,9]])
+y=num.array([[5,2,3],[2,7,7],[9,6,4]])
+print("array1")
+print(x)
+print("array2")
+print(y)
+print("multiplication of arrays of same size")
+print(num.multiply(x,y))
+
+
+# In[ ]:
+
+
+
+
